3fbd28255f595f3592d5aa29296041d23660981f
Anastasia <anastasiaizotova0080@gmail.com>
2026-03-19 20:27:01 +0300

    commit22

--- catalog/views.py
+++ catalog/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 def contacts(request):
-    return render(request, 'contact.html')
+    return render(request, 'contacts.html')
